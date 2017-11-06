9903fe37af1db87528bee37584e72edc38ed23fc
Quinlan Jager <quinlan.jager@gmail.com>
2017-11-06 21:57:22 +0000

    Added initial getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -3,3 +3,20 @@ const chalk = require('chalk');
 console.log(chalk.green("*******************************************"))
 console.log(chalk.green("* Welcome to the GitHub Avatar Downloader *"))
 console.log(chalk.green("*******************************************"))
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  const options = {
+    url : `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    'user-agent' : 'quinlanjager',
+  }
+  request(options, (err, response, body) => {
+    cb(err, body);
+  });
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
