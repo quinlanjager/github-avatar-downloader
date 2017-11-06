6f88009c574415e6c3104d216e0e399eb184f9d2
Quinlan Jager <quinlan.jager@gmail.com>
2017-11-06 21:44:04 +0000

    Added requirements for request and chalk.

---
+++ download_avatars.js
@@ -0,0 +1,5 @@
+const request = require('request');
+const chalk = require('chalk');
+console.log(chalk.green("*******************************************"))
+console.log(chalk.green("* Welcome to the GitHub Avatar Downloader *"))
+console.log(chalk.green("*******************************************"))
