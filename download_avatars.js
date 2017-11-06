7e426fec848fbb21a4711d9a81d31bb1b437213e
Quinlan Jager <quinlan.jager@gmail.com>
2017-11-06 23:47:58 +0000

    Linting files

--- download_avatars.js
+++ download_avatars.js
@@ -1,23 +1,22 @@
 // load dependencies
 const request = require('request');
 const fs = require('fs');
-const TOKEN = require('./secrets').GITHUB_TOKEN; // github token
-const chalk = require('chalk');
+const chalk = require('chalk'); // for styling
+const token = require('./secrets').GITHUB_TOKEN; // github token
 
 // load commandline arguments
-
-const REPOOWNER = process.argv[2];
-const REPONAME = process.argv[3];
+const repoOwner = process.argv[2];
+const repoName = process.argv[3];
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
   const options = {
     url : `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers : {
-    'User-Agent' : 'request',
-    'Authorization' : `token ${TOKEN}`
+      'User-Agent' : 'GitHub Avatar Downloader',
+      'Authorization' : `token ${token}`
     }
-  }
+  };
   request(options, (err, response, body) => {
     cb(err, body);
   });
@@ -27,27 +26,21 @@ function downloadImageByURL(url, path){
   // begin downloading image from given url
   request.get(url)
   .on('error', (error) => {
-    throw err;
+    throw error;
   })
-  .on('response', () => {
-    console.log(chalk.green("Response received."));
-  }).pipe(fs.createWriteStream(path))
-  .on('finish', () => {
-    console.log(chalk.green("Finished downloading image!"));
-  });
+  .pipe(fs.createWriteStream(path));
 }
 
-console.log(chalk.green("*******************************************"))
-console.log(chalk.green("* Welcome to the GitHub Avatar Downloader *"))
-console.log(chalk.green("*******************************************"))
+console.log(chalk.green('Welcome to the GitHub Avatar Downloader'));
 
-if(!REPOOWNER || !REPONAME){
+if(!repoOwner || !repoName){
   console.log(chalk.red('Please input a repository owner and a repository name using the following format:\n'), '<repo owner>', '<repo name>');
 } else {
-  getRepoContributors(REPOOWNER, REPONAME, function(err, result) {
+  console.log(chalk.green('Downloading GitHub avatars'));
+  getRepoContributors(repoOwner, repoName, function(err, result) {
     const parsedResult = JSON.parse(result); // Parse JSON result
-    for(let userID in parsedResult){ // find UserID
-      let user = parsedResult[userID];
+    for(const userID in parsedResult){ // find UserID
+      const user = parsedResult[userID];
       downloadImageByURL(user.avatar_url, `avatars/${user.login}.jpg`);
     }
   });
