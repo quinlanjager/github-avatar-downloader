0cf575cdfa95a51c5ef4f076294b24bc9edcd733
Quinlan Jager <quinlan.jager@gmail.com>
2017-11-07 04:29:04 +0000

    Cleaning up files further, adding a version using HTTPS module that dynamically inserts the file extension.

--- download_avatars_https.js
+++ download_avatars_https.js
@@ -1,49 +1,53 @@
 // load dependencies
 require('dotenv').config();
-const request = require('request');
 const fs = require('fs');
+const mime = require('mime');
+const https = require('https');
 const chalk = require('chalk'); // for styling
 const token = process.env.GITHUB_TOKEN; // github token
-
 
 // load commandline arguments
 const repoOwner = process.argv[2];
 const repoName = process.argv[3];
 
-
 function getRepoContributors(repoOwner, repoName, cb) {
   const options = {
-    url : `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    hostname : 'api.github.com',
+    path : `/repos/${repoOwner}/${repoName}/contributors`,
     headers : {
       'User-Agent' : 'GitHub Avatar Downloader',
       'Authorization' : `token ${token}`
     }
   };
-  request(options, (err, response, body) => {
-    if(response.statusCode === 401){
+  https.get(options, (res) => {
+    res.setEncoding = 'utf8';
+    if(res.statusCode === 401){
       console.log(chalk.red('Bad credentials given. Verify token in .env file.'));
-    } else if(response.statusCode === 404){
+    } else if(res.statusCode === 404){
       console.log(chalk.red('Given repository owner and/or repository owner could not be found.'));
     } else {
-      cb(err, body);
+      let body = "";
+
+      // Collect body
+      res.on('data', (chunk)=>{
+        body += chunk;
+      });
+      res.on('end', (err) => {
+        cb(body);
+      });
     }
   });
 }
 
-
 // write this with the HTTP module so that we can get the header before piping.
 function downloadImageByURL(url, path){
   // begin downloading image from given url
-  request.get(url)
-  .on('error', (error) => {
-    console.log(error);
-    throw error;
-  })
-  .pipe(fs.createWriteStream(path)).on('error', (error) => {
-    // file foldern not found
-    if(error.code === "ENOENT"){
-      fs.mkdir('avatars');
-    }
+  https.get(url, (res) => {
+    const fileExt = mime.getExtension(res.headers['content-type']);
+    res.on('error', (err) => {
+      throw error;
+    });
+    res.pipe(fs.createWriteStream(`${path}.${fileExt}`))
   });
 }
 
@@ -57,14 +61,20 @@ if(!token){
   console.log(chalk.red('Required argument missing. Input repository owner and repository name in the following format:\n'), '<repo owner>', '<repo name>');
 } else {
   console.log(chalk.green('Downloading GitHub avatars'));
-  getRepoContributors(repoOwner, repoName, function(err, result) {
+
+  // Check if 'avatars exists'
+  try{
+    fs.accessSync('avatars');
+  } catch (err) {
     if(err){
-      console.log(err);
+      fs.mkdir('avatars');
     }
+  }
+  getRepoContributors(repoOwner, repoName, function(result) {
     const parsedResult = JSON.parse(result); // Parse JSON result
     for(const userID in parsedResult){ // find UserID
       const user = parsedResult[userID];
-      downloadImageByURL(user.avatar_url, `avatars/${user.login}.jpg`);
+      downloadImageByURL(user.avatar_url, `avatars/${user.login}`);
     }
   });
 }
